0853d1db15903efe1fcc652ddd9b4f9ba144d079
Levi <suiyun@me.com>
2014-05-14 14:44:26 +0800

    Initial Commit

---
+++ BirthDay_soft/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BirthDay_soft
+//
+//  Created by YuLei on 14-5-14.
+//  Copyright (c) 2014年 ___DuanYuLei___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
